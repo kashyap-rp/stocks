606c414da80b6b56a2393c640aa7456f9a9f5d1b
Jordan Plows <jordan@lightapi.com>
2024-06-08 16:39:38 -0700

    dev: janitor commit

--- lib/alpha-finance/fetchChartData.ts
+++ lib/alpha-finance/fetchChartData.ts
@@ -3,29 +3,29 @@ import type {
   ChartOptions,
   ChartResultArray,
 } from "@/node_modules/yahoo-finance2/dist/esm/src/modules/chart"
-import type { Interval, Range } from "@/types/alpha-vantage"
-import { DEFAULT_RANGE, INTERVALS_FOR_RANGE, VALID_RANGES } from "./constants"
+import type { Interval} from "@/types/alpha-vantage"
+import { DEFAULT_TIME_PERIOD, FREQUENCIES_FOR_TIME_PERIOD, VALID_TIME_PERIODS } from "./constants"
 import { CalculateRange } from "@/lib/utils"
 import yahooFinance from "yahoo-finance2"
 
-export const validateRange = (range: string): Range =>
-  VALID_RANGES.includes(range as Range) ? (range as Range) : DEFAULT_RANGE
+export const validateRange = (range: string): string =>
+  VALID_TIME_PERIODS.includes(range) ? range : DEFAULT_TIME_PERIOD
 
-export const validateInterval = (range: Range, interval: Interval): Interval =>
-  INTERVALS_FOR_RANGE[range].includes(interval)
+export const validateInterval = (range: string, interval: Interval): Interval =>
+  FREQUENCIES_FOR_TIME_PERIOD[range].includes(interval)
     ? interval
-    : INTERVALS_FOR_RANGE[range][0]
+    : FREQUENCIES_FOR_TIME_PERIOD[range][0]
 
 export async function fetchChartData(
   ticker: string,
-  range: Range,
+  range: string,
   interval: Interval
 ) {
   noStore()
 
   const queryOptions: ChartOptions = {
     period1: CalculateRange(range),
-    interval: interval,
+    // interval: interval,
   }
 
   try {
