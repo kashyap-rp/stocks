a2b0e5e14b5b28959f9a49c0149e8ad4d780a4b9
Jordan Plows <jordan@lightapi.com>
2024-06-08 14:15:15 -0700

    dev: janitor commit

--- lib/alpha-finance/constants.ts
+++ lib/alpha-finance/constants.ts
@@ -1,15 +1,15 @@
-import type { Interval, Range } from "@/types/alpha-vantage"
+import type { Interval } from "@/types/alpha-vantage"
 
-export const DEFAULT_TICKER = "AAPL"
-export const DEFAULT_RANGE = "1d"
-export const DEFAULT_INTERVAL = "1m"
-export const DEFAULT_SCREENER = "most_actives"
+export const DEFAULT_SYMBOL = "AAPL"
+export const DEFAULT_TIME_PERIOD = "1d"
+export const DEFAULT_FREQUENCY = "1min"
+export const DEFAULT_SCREEN = "most_actives"
 
-export const VALID_RANGES: Range[] = ["1d", "1w", "1m", "3m", "1y"]
-export const INTERVALS_FOR_RANGE: { [key in Range]: Interval[] } = {
-  "1d": ["1m", "2m", "5m", "15m", "30m", "60m", "90m"],
-  "1w": ["1h", "1d", "5d"],
-  "1m": ["1h", "1d", "5d", "1wk"],
-  "3m": ["1d", "5d", "1wk", "1mo"],
-  "1y": ["1d", "5d", "1wk", "1mo", "3mo"],
+export const VALID_TIME_PERIODS: string[] = ["1d", "1w", "1m", "3m", "1y"]
+export const FREQUENCIES_FOR_TIME_PERIOD: { [key: string]: Interval[] } = {
+  "1d": ["1min", "5min", "15min", "30min", "60min"],
+  "1w": ["daily", "weekly"],
+  "1m": ["daily", "weekly", "monthly"],
+  "3m": ["daily", "weekly", "monthly"],
+  "1y": ["daily", "weekly", "monthly"],
 }
